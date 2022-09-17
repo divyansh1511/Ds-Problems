01547ffc6f94e9e5428b22f752c5cf4ff5b3836b
DIVYANSH GARG <dishugarg1511@gmail.com>
2022-09-17 15:28:29 +0530

    Stacks and Queues Done

--- Striver Sheet/7_Complete_Stack_and_Queue.cpp
+++ Striver Sheet/7_Complete_Stack_and_Queue.cpp
@@ -473,6 +473,130 @@ public:
     }
 };
 
+class LFUCache {
+public:
+    LFUCache(int capacity) {
+        this -> capacity = capacity;
+        size = 0;
+    }
+    
+    int get(int key) {
+        if (values.find(key) == values.end()) {
+            return -1;
+        }
+        update(key);
+        return values[key].first;
+    }
+    
+    void put(int key, int value) {
+        if (!capacity) {
+            return;
+        }
+        if (values.find(key) != values.end()) {
+            values[key].first = value;
+            update(key);
+        } else {
+            if (size == capacity) {
+                int evict = keys[lfu].front();
+                keys[lfu].pop_front();
+                values.erase(evict);
+                iters.erase(evict);
+            } else {
+                size++;
+            }
+            values[key] = {value, 1};
+            keys[1].push_back(key);
+            iters[key] = --keys[1].end();
+            lfu = 1;
+        }
+    }
+private:
+    int capacity, size, lfu = 0;
+    unordered_map<int, list<int>> keys;
+    unordered_map<int, pair<int, int>> values;
+    unordered_map<int, list<int>::iterator> iters;
+    
+    void update(int key) {
+        int freq = values[key].second;
+        auto iter = iters[key];
+        values[key].second++;
+        keys[freq].erase(iter);
+        keys[freq + 1].push_back(key);
+        iters[key] = --keys[freq + 1].end();
+        if (keys[lfu].empty()) {
+            lfu++;
+        }
+    }
+};
+
+void printMaxOfMin(int arr[], int n)
+{
+    stack<int> s;
+    int left[n+1]; 
+    int right[n+1];
+    for (int i=0; i<n; i++)
+    {
+        left[i] = -1;
+        right[i] = n;
+    }
+    for (int i=0; i<n; i++)
+    {
+        while (!s.empty() && arr[s.top()] >= arr[i])
+            s.pop();
+ 
+        if (!s.empty())
+            left[i] = s.top();
+ 
+        s.push(i);
+    }
+    while (!s.empty())
+        s.pop();
+
+    for (int i = n-1 ; i>=0 ; i-- )
+    {
+        while (!s.empty() && arr[s.top()] >= arr[i])
+            s.pop();
+ 
+        if(!s.empty())
+            right[i] = s.top();
+ 
+        s.push(i);
+    }
+
+    int ans[n+1];
+    for (int i=0; i<=n; i++)
+        ans[i] = 0;
+
+    for (int i=0; i<n; i++)
+    {
+        int len = right[i] - left[i] - 1;
+        ans[len] = max(ans[len], arr[i]);
+    }
+
+    for (int i=n-1; i>=1; i--)
+        ans[i] = max(ans[i], ans[i+1]);
+
+    for (int i=1; i<=n; i++)
+        cout << ans[i] << " ";
+}
+
+int celebrity(vector<vector<int> >& M, int n) 
+{
+    int left=0, right=n-1;
+    while (left < right) {
+        if (M[left][right] == 1) left++;
+        else right--;
+    }
+       
+    int potentialCandidate=left;
+       
+    for (int i=0; i<n; i++) {
+        if (i != potentialCandidate) {
+            if (M[potentialCandidate][i] == 1 || M[i][potentialCandidate] == 0) return -1;    
+        }
+    }   
+    return potentialCandidate;
+}
 
 int main(){
     int arr[] = {11 ,13 , 21 , 3};
